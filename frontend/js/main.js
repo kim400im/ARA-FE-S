7ec6cc7e1237060d0ef08662276df69a90293a5e
[GitHub <[GitHub>
2025-02-04 16:14:06 +0900

    css 모듈화

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,6 @@
 // main.js
 import { initializeEventHandlers } from './event-handlers.js';
-import { loadChatroomData } from './chat-actions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initializeEventHandlers();
-  loadChatroomData(); // 페이지 로드 시 채팅방 데이터 로드
 });
